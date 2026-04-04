d40b5030026e853f6f27d21ed08ecba857514a5a
Alan McClenaghan <alandmcclenaghan@icloud.com>
2026-04-04 11:41:15 +0100

    Logout API and it’s integration

--- src/modules/components/navbar/navbar.js
+++ src/modules/components/navbar/navbar.js
@@ -1,4 +1,5 @@
 import { LightningElement, api } from "lwc";
+const SERVER_URL = "http://localhost:3002"
 
 export default class Navbar extends LightningElement {
 
@@ -6,6 +7,10 @@ export default class Navbar extends LightningElement {
 
     get userName() {
         return this.loggedInUser ? this.loggedInUser.name : ''
+    }
+
+    get logoutUrl(){
+        return `${SERVER_URL}/oauth2/logout`
     }
 
 }
